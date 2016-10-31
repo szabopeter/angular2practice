0d9411745e8ac8762d5f7716746135bea2dfa088
zs-zs <zsolt.zsigmondi@hotmail.com>
2016-10-31 23:08:01 +0100

    api endpoint to post a score
    can be tested using cURL:
    
    curl -vX POST http://localhost:4000/score -d '{"score1":8,"score2":10}' \ --header "Content-Type: application/json"

--- api-server.js
+++ api-server.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var store = require('json-fs-store')();
+
+app.use(bodyParser.json());
+
+app.post('/score', function (req, res) {
+	console.log(req.body);
+	var score = { 
+		score1: req.body.score1, 
+		score2: req.body.score2 
+	};
+
+	store.add(score, function(err) {
+		if (err) {
+			throw err;
+		}
+	});
+	res.sendStatus(200);
+});
 
 app.get('/scores', function (req, res) {
 	store.list(function(err, scores) {
