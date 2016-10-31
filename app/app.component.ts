2b99507fe3f8bd333a46afd0eeb3cc3f5cc11389
zs-zs <zsolt.zsigmondi@hotmail.com>
2016-11-01 00:20:28 +0100

    app.component push & pull scores from server

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { Score } from './score';
+import { ScoreRepositoryService } from './services/scoreRepositoryService';
 
 @Component({
-  selector: 'app',
-  templateUrl: 'app/app.component.html',
+	selector: 'app',
+	templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
-    scores: Score[] = [];
+	scores: Score[] = [];
+	scoreRepository : ScoreRepositoryService;
+	
+	constructor(scoreRepositoryService: ScoreRepositoryService) {
+		this.scoreRepository = scoreRepositoryService;
+	}
 
-    saveScore(score:Score) : void {
-        this.scores.push(score)
-        console.log(this.scores);
-    }
-} 
+	ngOnInit() {
+		this.loadScoresFromServer();
+	}
+
+	loadScoresFromServer() {
+		this.scoreRepository
+			.getAll()
+			.then((scoresFromServer) => this.scores = scoresFromServer);
+	}
+
+	saveScore(score:Score) : void {
+		this.scores.push(score);
+		this.scoreRepository.add(score)
+			.then(this.loadScoresFromServer);
+	}
+}
