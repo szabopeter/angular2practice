d0323a021d5ed98a1626dfecd970e6e8a02837e6
zs-zs <zsolt.zsigmondi@hotmail.com>
2016-10-31 21:35:08 +0100

    improve naming + more meaningful property on gameForm

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent}   from './app.component';
-import { GameComponent } from './game.component';
+import { GameFormComponent } from './gameForm.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, GameComponent ],
+  declarations: [ AppComponent, GameFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
